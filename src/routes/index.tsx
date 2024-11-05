2b4cedb984818cf54b090c364e2661fd94326068
Justin <justin.scopelleti@gmail.com>
2024-11-04 22:29:18 -0500

    Broken modal component animation/transition

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,36 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Modal } from "@qwik-ui/headless";
 
 export default component$(() => {
+  useStyles$(`
+.modal-trigger {
+  background-color: grey;
+  transition: background-color 2s;
+}
+
+.modal-trigger:hover {
+  background-color: green;
+}
+
+.modal-panel[data-closing] {
+  opacity: 0;
+}
+
+.modal-panel {
+  opacity: 1;
+  transition: opacity 2s;
+}
+`);
+
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      <Modal.Root>
+        <Modal.Trigger class="modal-trigger">Open Modal</Modal.Trigger>
+        <Modal.Panel class="modal-panel">
+          <Modal.Close class="modal-trigger">Close Modal</Modal.Close>
+        </Modal.Panel>
+      </Modal.Root>
     </>
   );
 });
